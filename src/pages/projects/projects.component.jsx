47dc56dd657f682a860f0006d70cf9687619e175
Shubham <sk246903@gmail.com>
2020-08-08 10:00:36 -0700

    Updated projects page

--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Divider from '@material-ui/core/Divider';
 
 const Projects = () => {
     return (
@@ -10,10 +11,12 @@ const Projects = () => {
             <Typography variant='h4' align='left' gutterBottom style={{marginLeft:'17px'}}>Featured <span style={{color:'#EF798A'}}>Projects</span></Typography>
             <Grid container spacing={3} justify='center'>
                 <Grid item xs={12} sm={12} md={6} lg={4} align='center'>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '23rem' }}>
                     <Card.Img variant="top" src={require('../../assets/project.png')} />
+                    <Divider />
                     <Card.Body>
                         <Card.Title>Project Title</Card.Title>
+                        <Divider />
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
@@ -24,10 +27,12 @@ const Projects = () => {
                 </Grid>
 
                 <Grid item xs={12} sm={12} md={6} lg={4} align='center'>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '23rem' }}>
                     <Card.Img variant="top" src={require('../../assets/project.png')} />
+                    <Divider />
                     <Card.Body>
                         <Card.Title>Project Title</Card.Title>
+                        <Divider />
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
@@ -38,10 +43,12 @@ const Projects = () => {
                 </Grid>
 
                 <Grid item xs={12} sm={12} md={6} lg={4} align='center'>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '23rem' }}>
                     <Card.Img variant="top" src={require('../../assets/project.png')} />
+                    <Divider />
                     <Card.Body>
                         <Card.Title>Project Title</Card.Title>
+                        <Divider />
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
